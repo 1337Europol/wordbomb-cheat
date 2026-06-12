9804d7b25c579ddf9dfb889dcd01d67856651ddb
Europol <290176053+1337Europol@users.noreply.github.com>
2026-06-13 00:01:29 +0200

    Update site-config.js

--- src/client/js/site-config.js
+++ src/client/js/site-config.js
@@ -1,6 +1,6 @@
 /**
  * config public du site
- * la clé mail reste côté serveur (.env) jamais ici
+ * la clef mail reste coté serveur .env jamais ici tu captes ?
  */
 export const SITE_CONFIG = {
     githubUrl: 'https://github.com/1337Europol/wordbomb',
