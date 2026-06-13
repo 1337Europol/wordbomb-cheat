725715f64f2e2a22e75d968154a10f58b5e29c50
Europol <290176053+1337Europol@users.noreply.github.com>
2026-06-13 04:17:21 +0200

    Update client-id.js

--- src/server/lib/client-id.js
+++ src/server/lib/client-id.js
@@ -3,7 +3,7 @@
 const RE_UUID = /^[0-9a-f]{8}-[0-9a-f]{4}-4[0-9a-f]{3}-[89ab][0-9a-f]{3}-[0-9a-f]{12}$/i;
 
 /**
- * @param {unknown} value
+ * @param {unknown} valeur lol
  * @returns {boolean}
  */
 function isValidClientId(value) {
